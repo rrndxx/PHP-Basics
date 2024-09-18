0b3e2ae5530e1a8e13475d7b654854ffe5c45b04
Rendyll Ryan Cabardo <rendyllcabardo11@gmail.com>
2024-09-18 17:30:05 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,26 @@
+<?php
 
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbname = "sampledatabase";
+
+    $db = "mysql:host=$host;dbname=$dbname";
+
+    $connection = new PDO($db,$user,$pass);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+    $id = 20;
+
+//DELETE A DATA
+    $sql = "DELETE FROM students_table WHERE ID = ?";
+    $stmnt = $connection->prepare($sql);
+    $stmnt->execute([$id]);
+
+//SELECT ALL DATA
+    $stmnt = $connection->query("SELECT * FROM students_table");
+    
+    while($row = $stmnt->fetch()){
+    echo $row['ID']. "  ".$row['firstname']. "  ".$row['lastname']. "  ".$row['age']. "  ".$row['gender']. "<br>";
+}
+?>
