4d865d81d069a59a70c0365d97607c360f32460e
Rendyll Ryan Cabardo <rendyllcabardo11@gmail.com>
2024-09-25 16:22:05 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,21 @@
+<?php
+    $newConnection = new Connection();
 
+    Class Connection{
+        private $server = "mysql:host=localhost";
+        private $user = "root";
+        private $pass = "";
+        private $dbname = "sampledatabase";
+        private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO :: FETCH_OBJ);
+        protected $con;
+
+        public function openConnection()
+        {
+            try{
+                $this->con = new PDO($this->server,$this->user,$this->pass,$this->options);
+                return $this->con;
+            }catch(PDOException $th){
+                echo "There is a problem in the connection:".$th->getMessage();
+            }
+        }
+    }
